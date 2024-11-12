596a5089781ce42e1d48605f37d32f2bba642bd5
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-11-12 07:54:05 -0500

    Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def calculator(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1/num2
